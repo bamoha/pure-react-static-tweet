290ab68bae566385f9306cbf25afad7d58d7a269
Bash <bashgrey@greymatecare.com>
2019-08-27 15:24:29 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Tweet() {
+    return (
+        <div className="tweet">
+            <Avatar />
+            <div className="content">
+                <Author />
+                <Time />
+                <Message />
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetweetButton />
+                    <LikeButton />
+                    <MoreOptionsButton />
+                </div>
+            </div>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Avatar() {
+    return (
+        <img src="https://www.gravatar.com/avatar/nothing" alt="Avatar" className="avatar" />
+    )
+}
+
+function Message() {
+    return (
+        <div className="message">
+            Nigeria is a blessed country with corrupt leaders
+        </div>
+    )
+}
+
+function Author() {
+    return (
+        <span className="author" >
+            <span className="name" > Bashir Hamza </span>
+            <span className="handle" > @Bamoohaa </span>
+        </span>
+    )
+}
+
+const Time = () => (
+    <span className="time" > 3h ago </span>
+);
+
+const ReplyButton = () => (
+    <i className="fa fa-reply reply-button" />
+);
+
+const RetweetButton = () => (
+    <i className="fa fa-retweet retweet-button" />
+);
+
+const LikeButton = () => (
+    <i className="fa fa-heart like-button" />
+);
+
+const MoreOptionsButton = () => (
+    <i className="fa fa-ellipsis-h more-options-button" />
+);
+
+ReactDom.render(<Tweet />, document.querySelector('#root'))
